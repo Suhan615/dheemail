16842f2a7d62c1729a5eafd079df1160a45359a9
suhan615 <suhankulal725@gmail.com>
2026-05-14 13:33:11 +0530

    add the files

--- vercel_app.py
+++ vercel_app.py
@@ -1,5 +1,13 @@
 import os
+import sys
+from pathlib import Path
+
 from django.core.asgi import get_asgi_application
+
+BASE_DIR = Path(__file__).resolve().parent
+PROJECT_DIR = BASE_DIR / "dheemail"
+
+sys.path.insert(0, str(PROJECT_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dheemail.settings')
 app = get_asgi_application()
